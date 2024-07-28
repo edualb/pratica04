c8426e45f4019e17a3b7e35cac02e4f36ac4978d
eduardo <eduardoslafiq@gmail.com>
2024-07-28 18:00:59 +0100

    update env variable name

--- infra/variables.tf
+++ infra/variables.tf
@@ -16,7 +16,7 @@ variable "username" {
   description = "O usuario que vai ser usado para acessar a VM."
 }
 
-variable "vm_admin_password" {
+variable "VM_ADMIN_PASSWORD" {
   type        = string
   description = "A senha do usuario que vai ser usado para acessar a VM."
 }
