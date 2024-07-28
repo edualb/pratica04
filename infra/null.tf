9f282d593247cb5bdbfd40b49f310bf8554ef8d1
eduardo <eduardoslafiq@gmail.com>
2024-07-28 19:32:39 +0100

    debug

---
+++ infra/null.tf
@@ -0,0 +1,17 @@
+resource "null_resource" "ssh_connection" {
+  depends_on = ["azurerm_linux_virtual_machine.puc_minas"]
+
+  connection {
+    host     = azurerm_public_ip.puc_minas.ip_address
+    type     = "ssh"
+    port     = 22
+    password = var.vm_admin_password
+    user     = var.username
+    agent    = false
+    timeout  = "1m"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt-get -qq install python"]
+  }
+}
